697da33e9fad9078fe08db66634b3227ea0b2a4c
gourovdey <112324418+gourovdey@users.noreply.github.com>
2023-05-10 03:30:42 +0530

    Third Commit Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 const options = {
     method: 'GET',
-    headers: {'X-RapidAPI-Key': '1879196cb0msha21c9c10f0a5493p1552e0jsne5aa2c930959'}
+    headers: {
+      'X-RapidAPI-Key': '1879196cb0msha21c9c10f0a5493p1552e0jsne5aa2c930959',
+      'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
+    }
   };
   
-
+  
 const getWeather = (City) =>{
     askedCity.innerHTML = City.charAt(0).toUpperCase() + City.slice(1)
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+City, options)
+
+    fetch('https://yahoo-weather5.p.rapidapi.com/weather?format=json&u=c&location='+City, options)
     .then(response => response.json())
-    .then((response) => {
+    .then(response => {
         
+        //temperature
+        document.getElementById('temp').innerHTML = response.current_observation.condition.temperature
+        document.getElementById('weather-condition').innerHTML = response.current_observation.condition.text
+        sunrise.innerHTML = response.current_observation.astronomy.sunrise
+        sunset.innerHTML = response.current_observation.astronomy.sunset
 
-        /* cloud_pct.innerHTML = response.cloud_pct */
+        //humidity
+        humidity.innerHTML = response.current_observation.atmosphere.humidity
+        visibility.innerHTML = response.current_observation.atmosphere.visibility
+        pressure.innerHTML = response.current_observation.atmosphere.pressure
 
-        document.getElementById('temp').innerHTML = response.temp
-        /* temp.innerHTML = response.temp */
+        //wind
+        wind_speed.innerHTML = response.current_observation.wind.speed
+        wind_direction.innerHTML = response.current_observation.wind.direction
+        wind_chill.innerHTML = response.current_observation.wind.chill
 
-        feels_like.innerHTML = response.feels_like
-        humidity.innerHTML = response.humidity
-        min_temp.innerHTML = response.min_temp
-        max_temp.innerHTML = response.max_temp
-        wind_speed.innerHTML = response.wind_speed
-        wind_degrees.innerHTML = response.wind_degrees
-        sunrise.innerHTML = response.sunrise
-        sunset.innerHTML = response.sunset
     })
     .catch(err => console.error(err));
 }
+  
 
 mybuton.addEventListener("click", (e)=>{
     e.preventDefault()
@@ -34,26 +41,25 @@ mybuton.addEventListener("click", (e)=>{
     getWeather(city_name.value)
 })
 
-
 getWeather("Delhi")
-
 
 const otherCityWeather = (myCity, classname) =>{
 
-    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+myCity, options)
+    fetch('https://yahoo-weather5.p.rapidapi.com/weather?format=json&u=c&location='+myCity, options)
     .then(response => response.json())
-    .then((response) => {
+    .then(response => {
         
-
-        document.querySelectorAll("."+classname)[0].innerHTML = response.temp
-        document.querySelectorAll("."+classname)[1].innerHTML = response.max_temp
-        document.querySelectorAll("."+classname)[2].innerHTML = response.min_temp
-        document.querySelectorAll("."+classname)[3].innerHTML = response.humidity
-        document.querySelectorAll("."+classname)[4].innerHTML = response.wind_speed
-        document.querySelectorAll("."+classname)[5].innerHTML = response.feels_like
-        document.querySelectorAll("."+classname)[6].innerHTML = response.wind_degrees
-        document.querySelectorAll("."+classname)[7].innerHTML = response.sunrise
-        document.querySelectorAll("."+classname)[8].innerHTML = response.sunset
+        document.querySelectorAll("."+classname)[0].innerHTML = response.current_observation.condition.temperature
+        
+        document.querySelectorAll("."+classname)[1].innerHTML = response.current_observation.wind.speed
+        
+        document.querySelectorAll("."+classname)[2].innerHTML = response.current_observation.atmosphere.visibility
+        
+        document.querySelectorAll("."+classname)[3].innerHTML = response.current_observation.atmosphere.pressure
+        
+        document.querySelectorAll("."+classname)[4].innerHTML = response.current_observation.atmosphere.humidity
+        
+        document.querySelectorAll("."+classname)[5].innerHTML = response.current_observation.condition.text
     })
 }
 
@@ -63,37 +69,3 @@ otherCityWeather("Seoul", "sk")
 otherCityWeather("Kolkata", "kol")
 otherCityWeather("Sydney", "syd")
 otherCityWeather("Moscow", "msk")
-
-
-
-
-
-
-    
-
-
-    
-/*     const url = 'https://weather-by-api-ninjas.p.rapid2api.com/v1/weather?city=Delhi';
-    const options = {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/octet-stream',
-            'X-RapidAPI-Key': '1879196cb0msha21c9c10f0a5493p1552e0jsne5aa2c930959',
-            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-        }
-    };
-    
-
-
-async function test(){
-    
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-test() */
